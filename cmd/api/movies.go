01cd1d7aeff7ddb937a94ab7b9e4119bdc8daf1e
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2025-02-12 15:29:47 +0530

    Add movie handlers for creating and showing movies

---
+++ cmd/api/movies.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "create a new moveis")
+
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+
+	params := httprouter.ParamsFromContext(r.Context())
+
+	// We can then use the ByName() method to get the value of the "id" parameter from
+	// the slice. In our project all movies will have a unique positive integer ID, but
+	// the value returned by ByName() is always a string. So we try to convert it to a
+	// base 10 integer (with a bit size of 64). If the parameter couldn't be converted,
+	// or is less than 1, we know the ID is invalid so we use the http.NotFound()
+	// function to return a 404 Not Found response.
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "show details of the movie %d\n ", id)
+}
